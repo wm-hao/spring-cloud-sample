f9d5fd189d6f49ebb5944dfda7fa5b3bb88d70be
wm-hao <448826602@qq.com>
2018-06-12 08:04:38 +0800

    setting config server

--- spring-cloud-config-server/src/main/java/zx/sc/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/zx/sc/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -3,9 +3,11 @@ package zx.sc.springcloudconfigserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaServer
 public class SpringCloudConfigServerApplication {
 
 	public static void main(String[] args) {
